1f4ca2a5a02e196adc7f1e04fa0f9c0aa1c9623d
PhoenixMomof2 <89948037+PhoenixMomof2@users.noreply.github.com>
2021-09-16 17:53:13 -0400

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
-// Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"];
+
+function destructivelyAppendCat(){
+    cats.push("Ralph");
+    cats;
+}
+function destructivelyPrependCat(){
+    cats.unshift ("Bob"); 
+}
+function destructivelyRemoveLastCat(){
+    cats.pop ();
+}
+function destructivelyRemoveFirstCat(){
+    cats.shift();
+}
+function appendCat(name){
+   return [...cats,name]
+}
+function prependCat(name){
+    return [name, ...cats]
+}
+function removeLastCat(name){
+    return cats.slice(0, cats.length-1); 
+}
+function removeFirstCat(){
+    return cats.slice(1);
+}
